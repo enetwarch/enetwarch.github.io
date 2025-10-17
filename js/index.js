1c470b68ed194484006c11e6b583d9bb43e0ddb1
enetwarch <enetwarch@users.noreply.github.com>
2025-10-17 19:30:52 +0000

    deploy: 1f2312f00fffdf2f97da6c701cc8220fe487b599

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 import stack from "../data/stack.json" with { type: "json" };
 import blogs from "../data/blogs.json" with { type: "json" };
 import { addTechStack, addBlogPosts, linkDialog } from "./modules/components.js";
+import { addStarfieldCanvas } from "./modules/starfield.js";
 
 window.addEventListener("DOMContentLoaded", () => {
   addTechStack("tech-stack-list", "tech-stack-template", stack);
@@ -9,4 +10,6 @@ window.addEventListener("DOMContentLoaded", () => {
   linkDialog("hamburger-dialog", "hamburger-button");
   linkDialog("subscribe-dialog", "subscribe-button1");
   linkDialog("subscribe-dialog", "subscribe-button2");
+
+  addStarfieldCanvas("starfield-canvas", 250);
 });
